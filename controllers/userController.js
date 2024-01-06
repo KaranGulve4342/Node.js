0b2977e29e3256767fb6759fcbaae1fad4b441e9
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-06 16:46:47 +0530

    added bcrypt for hashing password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 
 // desc Register a user
@@ -7,6 +9,21 @@ const asyncHandler = require("express-async-handler");
 // acccess public
 
 const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User Already registered");
+    }
+
+    // hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+    console.log("hashed Password: ", hashedPassword);
+
     res.json({ message : "Register for user"});
 });
 
